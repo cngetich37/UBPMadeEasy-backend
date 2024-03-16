49364cf993e93e3b8c3257836eaf05736893fe6b
cngetich37 <cngetich37@gmail.com>
2024-03-16 07:35:44 +0300

    updated the get suggestions function

--- controllers/UBPController.js
+++ controllers/UBPController.js
@@ -39,9 +39,7 @@ const createUBP = asyncHandler(async (req, res) => {
 
   try {
     await UBP.create(naicsCategories);
-    res
-      .status(201)
-      .json({ message: "Business Activity added successfully" });
+    res.status(201).json({ message: "Business Activity added successfully" });
   } catch (error) {
     console.error("Error adding UBP Business Activity:", error);
     res.status(500).json({ message: "Internal server error" });
@@ -91,7 +89,7 @@ const getUbpDictionary = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc Get UBP Activities by Common Business Activity
+// @desc Get a UBP Activity by Common Business Activity
 // @route GET /api/naics/ubpdictionary/:commonBusinessActivity
 // @access public
 const getSuggestions = asyncHandler(async (req, res) => {
@@ -102,7 +100,7 @@ const getSuggestions = asyncHandler(async (req, res) => {
   const pluralRegex = new RegExp(`^${commonBusinessActivity}s?$`, "i");
 
   // Search for both singular and plural forms
-  const ubpActivities = await UBP.find(
+  const ubpActivity = await UBP.findOne(
     {
       $or: [
         { commonBusinessActivity: singularRegex },
@@ -112,14 +110,12 @@ const getSuggestions = asyncHandler(async (req, res) => {
     { commonBusinessActivity: 1, _id: 0 }
   ); // Include only the "commonBusinessActivity" field and exclude "_id"
 
-  if (ubpActivities.length === 0) {
+  if (!ubpActivity) {
     res.status(404).json({ message: "Business Activity not found" });
   } else {
-    // Extract only the "commonBusinessActivity" field from the documents
-    const activities = ubpActivities.map((activity) => ({
-      commonBusinessActivity: activity.commonBusinessActivity,
-    }));
-    res.status(200).json(activities);
+    // Extract only the "commonBusinessActivity" field from the document
+    const { commonBusinessActivity } = ubpActivity;
+    res.status(200).json({ commonBusinessActivity });
   }
 });
 
