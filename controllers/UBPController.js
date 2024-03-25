4e4452f476d321dfad423bbd97b135fa3ece6166
cngetich37 <cngetich37@gmail.com>
2024-03-25 23:26:41 +0300

    updated the UBPController

--- controllers/UBPController.js
+++ controllers/UBPController.js
@@ -34,7 +34,7 @@ const UBPFinanceAct = asyncHandler(async (req, res) => {
       for (let key in obj) {
         if (obj[key] === undefined) {
           obj[key] = "0";
-        } else if (typeof obj[key] === "object" && obj[key] !== null) {
+        } else if (typeof obj[key] === 'object') {
           replaceUndefined(obj[key]);
         }
       }
@@ -42,15 +42,14 @@ const UBPFinanceAct = asyncHandler(async (req, res) => {
     };
 
     // Filter out undefined strings and replace them with "0"
-    const filteredFinance = ubpFinance.map((item) => {
-      const filteredItem = replaceUndefined(item.toObject());
-      return filteredItem;
+    const filteredFinance = ubpFinance.map(item => {
+      return replaceUndefined(item.toObject());
     });
 
     res.status(200).json(filteredFinance);
   } catch (error) {
     // Handle errors
-    console.error("Error in UBPFinanceAct:", error);
+    console.error(error);
     res.status(500).json({ message: "Server Error" });
   }
 });
