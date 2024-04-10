4f886208438c7a1d1689c2148fd4a25a53e4ecac
cngetich37 <cngetich37@gmail.com>
2024-04-10 13:04:28 +0300

    removed the contact functionality

--- routes/ContactRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { contactUs } = require("../controllers/ContactController");
-const router = express.Router();
-
-router.route("/send-email").post(contactUs);
-
-module.exports = router;
