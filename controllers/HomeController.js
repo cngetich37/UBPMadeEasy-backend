c4f7723a878ff624ada37c686c6149e2913cff23
cngetich37 <cngetich37@gmail.com>
2025-01-29 18:57:47 +0300

    Enhance HomeController styles and structure for improved layout and user experience

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -10,14 +10,27 @@ const getHome = (req, res) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Welcome to UBPMadeEasy API</title>
         <style>
-          body { font-family: Arial, sans-serif; text-align: center; margin: 50px; background-color:#065A38}
+          body { 
+            font-family: Arial, sans-serif; 
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            background-color: #065A38;
+          }
+          .container {
+            text-align: center;
+          }
           h1 { color: #FBCC1A; }
           p { color: #FBCC1A; }
         </style>
       </head>
       <body>
-        <h1>Welcome to the UBPMadeEasy API</h1>
-        <p>Navigate UBP Easily!</p>
+        <div class="container">
+          <h1>Welcome to the UBPMadeEasy API</h1>
+          <p>Your API is up and running!</p>
+        </div>
       </body>
       </html>
     `;
